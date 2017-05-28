64e6e5f50b988b04ff0c185da954012b5827a790
Paulo Louro <pjc@au2mate.dk>
2017-05-28 09:51:25 +0200

    Add project files.

---
+++ LipoMonitorI2C/LipoMonitorI2C.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		LipoMonitorI2C.ino
+ Created:	5/28/2017 9:51:23 AM
+ Author:	pjc
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
